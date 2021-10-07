0dd6ffe9796e7bdf48fe1aa8739d5c9f3e20d30d
sijab <mar.jablonski96@wp.pl>
2021-10-07 12:12:14 +0200

    cleancode

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <AppContextProvider>
-    <App />
+      <App />
     </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
